373ee07dce1dddde502846f74eb2ae9b4db07cb8
Rashid Shahariar <118152985+rashid-shahariar@users.noreply.github.com>
2024-05-19 12:17:06 +0600

    Add files via upload
    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+                   .AllowAnyMethod()
+                   .AllowAnyHeader();
+        });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseAuthorization();
+
+app.MapControllers();
+app.UseCors("AllowAll");
+
+app.Run();
